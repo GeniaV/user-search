837a9913d6c9208a3743db3e004bdb6c21c70acb
unknown <evgenia.verhovtseva@yandex.ru>
2023-07-31 20:25:26 +0300

    add: more responsiveness for mobile devices

--- src/components/user-details/user-details.tsx
+++ src/components/user-details/user-details.tsx
@@ -5,12 +5,13 @@ export function UserDetails() {
   const userDetails = useAppSelector(state => state.userDetailsReducer.details);
 
   return (
-    <>
+    <div className={styles.cont}>
       <img className={styles.avatar} src={userDetails.avatar_url} alt="Avatar" />
-      <h2 className={styles.title}><b>Пользователь:&nbsp;&#32;</b>{userDetails.login}</h2>
-      <p className={styles.text}><b>Локация:&nbsp;&#32;</b>{userDetails.location}</p>
-      <p className={styles.text}><b>Количество подписчиков:&nbsp;&#32;</b>{userDetails.followers}</p>
-      <p className={styles.text}><b>Количество публичных репозиториев:&nbsp;&#32;</b>{userDetails.public_repos}</p>
-    </>
+      <h2 className={styles.title}><b>Пользователь:&nbsp;</b>{userDetails.login}</h2>
+      <p className={styles.text}><b>Локация:&nbsp;</b>{userDetails.location}</p>
+      <p className={styles.text}><b>Количество подписчиков:&nbsp;</b>{userDetails.followers}</p>
+      <p className={styles.text}><b>Количество публичных репозиториев:&nbsp;</b>{userDetails.public_repos}</p>
+    </div>
   )
 };
+
